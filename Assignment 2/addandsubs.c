54aeb95a6e38ee7ee28b6f5ecc16084b5b18741d
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-02-16 02:01:34 +0530

    Create addandsubs.c

---
+++ Assignment 2/addandsubs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a, b, c, d;
+    float p, q, r, s;
+printf("a=");
+scanf("%d",&a);
+printf("b=");
+scanf("%d",&b);
+printf("p=");
+scanf("%f",&p);
+printf("q=");
+scanf("%f",&q);
+    c = a + b;
+    d = a - b;
+    r = p + q;
+    s = p - q;
+
+    printf("%d\n",c);
+     printf("%d\n",d);
+      printf("%f\n",r);
+       printf("%f\n",s);
+
+    return 0;
+}
